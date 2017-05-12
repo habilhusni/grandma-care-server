a6d25ec4be759ea0afdf142f6c7b49fdbbbf5e1e
arfanizar <arfanizar.fth@gmail.com>
2017-05-12 16:09:38 +0700

    add aws IOT methods

--- app.js
+++ app.js
@@ -8,11 +8,30 @@ const jwt 					= require('jsonwebtoken');
 const passwordHash 	= require('password-hash');
 const mongoose 			= require('mongoose');
 const logger 				= require('morgan');
+const awsIot 				= require('aws-iot-device-sdk');
 require('dotenv').config()
 
 const app 					= express();
 const Users 				= require('./models/user');
 const index 				= require('./routes/index');
+const device 				= awsIot.device({
+   keyPath: './device-accelerometer.private.key',
+  certPath: './device-accelerometer.cert.pem',
+    caPath: './root-CA.crt',
+      host: 'a38x4799nd8aym.iot.eu-central-1.amazonaws.com',
+    region: 'eu-central-1'
+});
+
+device
+  .on('connect', function() {
+    console.log('aws iot connected');
+    device.subscribe('topic_1');
+    });
+
+device
+  .on('message', function(topic, payload) {
+    console.log('message', topic, payload.toString());
+  });
 
 mongoose.Promise = global.Promise
 app.use(cors())
@@ -21,7 +40,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 const dbconfig = {
-	development:'mongodb://localhost/final-project',
+	development:'mongodb://localhost/final-project-dev',
 	test:'mongodb://localhost/final-project-test',
 	production: 'mongodb://localhost/final-project-prod'
 }
